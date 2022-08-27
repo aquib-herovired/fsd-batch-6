547bf2bfa0015a0b721a0c09906feb4f832da2f6
aquib-herovired <103304126+aquib-herovired@users.noreply.github.com>
2022-08-27 18:14:42 +0530

    Add files via upload

---
+++ 27th August/scripts.js
@@ -0,0 +1,37 @@
+// Conditional Rendering
+
+10 < 5 ? console.log("True") : console.log("False");
+
+
+// Dynamically Typed Languages
+// var salary = 1000.23;
+// salary = 10000;
+// var isPaidUser = false;
+// var userName = "Kate";
+// var gender = "F";
+
+// const discount = 20;
+
+// {
+//     let userSalary = 1000;
+//     console.log(userSalary);
+// }
+
+// let userEmail = "kate@email.com";
+
+// var salary = 10000;
+
+// function addNumbers() {
+//     var salary = 20000;
+//     console.log(salary);
+// }
+
+// function subtractNumbers() {
+//     var salary = 30000;
+//     console.log(salary);
+// }
+
+// addNumbers();
+// subtractNumbers();
+
+
